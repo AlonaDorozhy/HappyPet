0454c8b7bc8884a69eb499cad5f45c8ef059a39f
Alona <dorozhynskaad@gmail.com>
2019-09-28 03:53:47 +0300

    first  HTML

---
+++ main.js
@@ -0,0 +1,35 @@
+$(function () {
+  $("#test").click(function () {
+    $(".test").modal('show');
+  });
+  $(".test").modal({
+    closable: true
+  });
+});
+console.log("hi");
+$(function () {
+  $("#test1").click(function () {
+    $(".test1").modal('show');
+  });
+  $(".test1").modal({
+    closable: true
+  });
+});
+console.log("hi");
+$(function () {
+  $("#test2").click(function () {
+    $(".test2").modal('show');
+  });
+  $(".test2").modal({
+    closable: true
+  });
+});
+console.log("hi");
+$(function () {
+  $("#test3").click(function () {
+    $(".test3").modal('show');
+  });
+  $(".test2").modal({
+    closable: true
+  });
+});
